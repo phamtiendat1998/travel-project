1613b7896ba7f942975a2a67688b598e0f72829e
Pham Tien Dat <dattp@dragonfroot.com>
2019-04-22 17:33:39 +0700

    create component struct

--- server/app.js
+++ server/app.js
@@ -11,6 +11,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested, Content-Type,Accept,Authorization');
+    if (req.method === 'OPTION') {
+        res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+        return res.status(200).json({});
+    }
+    next();
+});
 // Routes which should handle requests
 app.use('/users', userRouter);
 
---
+++ src/components/page/Intro/intro.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './_intro.scss'
+
+export default class intro extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
